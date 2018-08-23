5b1b6ed8b29bc4742d47b50ddb1769b606a5a183
Kevin <kevelberger@gmail.com>
2018-08-22 21:52:33 -0700

    Use HashRouter over BrowserRouter

--- app/src/renderer.js
+++ app/src/renderer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './global.css';
 
 render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('app')
 )
