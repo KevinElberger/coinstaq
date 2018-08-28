a7c29e402ef584f82b860fc72c1a56bd083ba228
Kevin <kevelberger@gmail.com>
2018-08-27 20:30:09 -0700

    Redirect default route to dashboard

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import PageWrapper from './components/PageWrapper/PageWrapper';
 import Header from './components/Header/Header';
 import Dashboard from './components/Dashboard/Dashboard';
@@ -13,6 +13,9 @@ export default class App extends Component {
       <div>
         <Header />
           <Switch>
+            <Route exact path="/" render={() => (
+              <Redirect to="/dashboard" />
+            )}/>
             <Route path="/dashboard" exact component={PageWrapper(Dashboard)} />
             <Route path="/portfolio" component={PageWrapper(Portfolio)} />
             <Route path="/news" component={PageWrapper(News)} />
