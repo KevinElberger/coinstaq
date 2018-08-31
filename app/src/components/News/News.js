854aeedf76cfc3cbed087510623ae7f5893f6f39
Kevin <kevelberger@gmail.com>
2018-08-30 17:03:31 -0700

    Dynamically show when last news update was

--- app/src/components/News/News.js
+++ app/src/components/News/News.js
@@ -4,12 +4,15 @@ import { shell } from 'electron';
 import { getNews } from '../../utils/cryptoApi';
 import './styles.css';
 
+const prettyMs = require('pretty-ms');
+
 export default class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
-      loaded: false
+      loaded: false,
+      updateTime: new Date().getTime() - new Date().getTime()
     };
     this.handleLink = this.handleLink.bind(this);
   }
@@ -19,17 +22,21 @@ export default class News extends Component {
   }
 
   componentDidMount() {
+    const updateTime = new Date().getTime();
+
     getNews()
       .then(results => {
         this.setState({
           loaded: true,
-          articles: results
+          articles: results,
+          updateTime: new Date().getTime() - updateTime
         })
       });
   }
 
   render() {
     const { loaded } = this.state;
+    const updateTime = prettyMs(this.state.updateTime, { verbose: true });
     const articles = this.state.articles.map((item, i) => (
       <div key={item.id} className='article-wrapper' onClick={ () => { this.handleLink(item.url) }}>
         <div className='article'>
@@ -44,7 +51,7 @@ export default class News extends Component {
         <div className='news-header'>
           <h3>Recent News</h3>
           <Button circular icon='refresh' size='mini'></Button>
-          <span className='news-last-updated'>Last updated 12 minutes ago</span>
+          <span className='news-last-updated'>Last updated { updateTime } ago</span>
         </div>
         <div className='news-articles-wrapper'>
           { loaded ? 
